0ff6d41a7fd7eedd771e6dfac11bc93967a41eae
nils-matthaei <n.matthaei@proton.me>
2025-03-19 13:31:17 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!(
+        "Satellite 8 has sent bit 0 (delta = 72)
+Satellite 9 has sent bit 1 (delta = 449)
+Satellite 18 has sent bit 0 (delta = 345)
+Satellite 22 has sent bit 1 (delta = 157)"
+    );
+}
