8d4b7e6b151fc8dc7e6144dcf88ca87f2259f42a
Jakob Kohler <schasch.dev@protonmail.com>
2025-03-19 21:13:16 +0100

    vibin'

--- src/cdma/chipsequence.rs
+++ src/cdma/chipsequence.rs
@@ -5,18 +5,18 @@ pub struct Chipsequence {
 }
 
 impl Chipsequence {
-        fn new() -> Self {
+        pub fn new() -> Self {
                 Self { arr: [0, 0, 0, 0, 0, 0, 0, 0] }
         }
 
         // technically this flips a bit but we will only use it once per bit to set it
-        fn set_bit(&self, bit_index: usize) {
+        pub fn set_bit(&mut self, bit_index: usize) {
                 if bit_index > 1022 {
                         return;
                 }
                 let arr_index: usize = bit_index / 128;
                 let inner_bit_index: usize = bit_index - arr_index*128;
-                let mask: u128 = 1 << inner_bit_index;
-                self.arr[arr_index] & mask;
+
+                self.arr[arr_index] = self.arr[arr_index] ^ (1 << inner_bit_index)
         }
 }
