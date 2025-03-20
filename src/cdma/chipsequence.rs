6c7a79a2ea201fc79808a1392a46b39ab04f3b8d
nils-matthaei <n.matthaei@proton.me>
2025-03-20 09:42:35 +0100

    fixed some spaghetti

--- src/cdma/chipsequence.rs
+++ src/cdma/chipsequence.rs
@@ -9,7 +9,6 @@ impl Chipsequence {
                 Self { arr: [0, 0, 0, 0, 0, 0, 0, 0] }
         }
 
-        // technically this flips a bit but we will only use it once per bit to set it
         pub fn set_bit(&mut self, bit_index: usize) {
                 if bit_index > 1022 {
                         return;
@@ -17,6 +16,6 @@ impl Chipsequence {
                 let arr_index: usize = bit_index / 128;
                 let inner_bit_index: usize = bit_index - arr_index*128;
 
-                self.arr[arr_index] = self.arr[arr_index] ^ (1 << inner_bit_index)
+                self.arr[arr_index] |= 1 << inner_bit_index;
         }
 }
