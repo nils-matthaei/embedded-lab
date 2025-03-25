1164e4f12845a22551d5498b3ed3c63ec12211a7
nils-matthaei <n.matthaei@proton.me>
2025-03-25 18:19:19 +0100

    Implement function for calculating correlation product of chipset and signal
    also added get_chip helper function
    
    not sure if works

--- src/cdma/chipsequence.rs
+++ src/cdma/chipsequence.rs
@@ -19,10 +19,44 @@ impl Chipsequence {
                 self.arr[arr_index] |= 1 << inner_bit_index;
         }
 
+        pub fn get_chip(&self, chip_index: usize) -> Result<i32, &str> {
+                if chip_index > 1023 {
+                        return Err("chip_index must be in range 0..1023");
+                }
+
+                let arr_index: usize = chip_index / 128;
+                let inner_bit_index: usize = chip_index - arr_index*128;
+
+                let sub_sequence: u128 = self.arr[arr_index];
+                let chip: i32 = (sub_sequence >> inner_bit_index & 0x1) as i32; // precedence of >> stronger than that of &
+                Ok(chip)
+        }
+
         pub fn print_bitwise(&self) {
                 for &value in &self.arr {
                     // Print each u128 value in binary format
                     println!("{:0128b}", value);
                 }
+        }
+
+        pub fn correlation_product_with_signal(&self, mut signal: &Vec<i32>, delta: usize) -> Result<i32, &str>{
+                if signal.len() < 1023 { return Err("Signal must contain at least 1023 values."); }
+
+                let shifted_signal: Vec<i32> = {
+                        let mut signal_copy = signal.clone();
+                        signal_copy.rotate_left(delta); // evil O(n) operation
+                        signal_copy
+                };
+
+                let mut result: i32 = 0;
+
+                for i in 0..1023 {
+                        match self.get_chip(1023-i) {
+                            Ok(chip) => result += shifted_signal[i] + chip,
+                            Err(msg) => return Err(msg)
+                        }
+                }
+
+                return Ok(result);
         }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use cdma::{chipsequence::Chipsequence, gold_codes::{GoldCodeGenerator, REGISTER_SUMS}};
+use std::fs;
 
 mod cdma;
 fn main() {
@@ -7,5 +8,22 @@ fn main() {
 
         let seq: Chipsequence = gen.generate();
 
-        seq.print_bitwise();
+        // seq.print_bitwise();
+
+        let sig_string = fs::read_to_string("./signal")
+        .expect("Could not read file :(");
+
+        let signal: Vec<i32> = sig_string
+            .split(" ")
+            .filter_map(|s| s.trim().parse::<i32>().ok())
+            .collect();
+
+        for delta in 0..1024 {
+                let res = seq.correlation_product_with_signal(&signal, delta);
+
+                match res {
+                    Ok(product) => print!("correlation product with delta of {} is {}\n", delta, product),
+                    Err(msg) => print!("Error: {}\n", msg)
+                }
+        }
 }
