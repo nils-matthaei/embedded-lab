be8514076408a55a6a137c212275641d32821408
nils-matthaei <n.matthaei@proton.me>
2025-03-26 18:28:29 +0100

    Implement decoding of signal

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,50 @@
-use cdma::{chipsequence::Chipsequence, gold_codes::{GoldCodeGenerator, REGISTER_SUMS}};
-use std::fs;
-use std::collections::HashMap;
-
 mod cdma;
+
+use cdma::{chipsequence::{self, Chipsequence}, gold_codes::{GoldCodeGenerator, REGISTER_SUMS}};
+use std::fs;
+
+struct DecodedSignal {
+        sattelite_id: usize,
+        bit: i32,
+        delta: usize
+}
 fn main() {
-        // let regsum = REGISTER_SUMS[1];
-        // let mut gen: GoldCodeGenerator = GoldCodeGenerator::new(regsum);
-        // let seq: Chipsequence = gen.generate();
-
-        // seq.print_bitwise();
-
-        let sig_string = fs::read_to_string("./signal")
-        .expect("Could not read file :(");
+        // read signal-file and write values to vector
+        let args: Vec<String> = std::env::args().collect();
+        if args.len() < 2 {
+            eprintln!("Usage: {} <path_to_signal_file>", args[0]);
+            std::process::exit(1);
+        }
+        let sig_string = fs::read_to_string(&args[1])
+            .expect("Could not read file :(");
 
         let signal: Vec<i32> = sig_string
             .split(" ")
             .filter_map(|s| s.trim().parse::<i32>().ok())
             .collect();
 
-        let mut prod_map: HashMap<i32, i32> = HashMap::new();
-
+        // generate chipsequences
+        let mut chipsequences: Vec<Chipsequence> = Vec::new();
         for regsum in REGISTER_SUMS {
                 let mut gen: GoldCodeGenerator = GoldCodeGenerator::new(regsum);
-                let seq: Chipsequence = gen.generate();
-                for delta in 0..1023 {
-                        let res = seq.correlation_product_with_signal(&signal, delta);
+                chipsequences.push(gen.generate());
 
-                        match res {
-                            Ok(product) => {
-                                *prod_map.entry(product).or_insert(0) += 1;
-                            }
-                            Err(msg) => print!("Error: {}\n", msg)
-                        }
+        }
+
+        // decode signal
+        let mut DecodedSignals: Vec<DecodedSignal> = Vec::new();
+        for (index,sequence) in chipsequences.iter().enumerate() {
+                if let Some(result) = sequence.cross_correlate_with_signal(&signal) {
+                                DecodedSignals.push(DecodedSignal {
+                                                sattelite_id: index + 1,
+                                                bit: result.0,
+                                                delta: result.1
+                                });
                 }
         }
-        for (key, value) in &prod_map {
-                println!("Correlation Product: {}, Count: {}", key, value);
+
+        // print results
+        for dsig in DecodedSignals {
+                println!("Sattelite {} has sent bit {} (delta = {})", dsig.sattelite_id, dsig.bit, dsig.delta);
         }
 }
